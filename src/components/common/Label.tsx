bdde8733a44afa95623f248f00154016b3eda93c
유림 <yulimh001@gmail.com>
2025-12-30 15:41:09 +0900

    refactor: 폰트 스타일 직접 받도록 수정

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -2,18 +2,20 @@
 
 import React from 'react'
 import * as Style from '@/styles/common/Label.styles'
+import { TypographyType } from '@/styles/emotion'
 
 interface LabelProps {
   isRequired?: boolean
   children: React.ReactNode
   labelElement?: React.ReactNode // label 과 같이있는 버튼 컴포넌트
+  labelStyle?: TypographyType
 }
 
-export default function Label({ children, labelElement, isRequired = false }: LabelProps) {
+export default function Label({ children, labelElement, isRequired = false, labelStyle }: LabelProps) {
   return (
     <Style.ContainerRow>
       <Style.Row>
-        <Style.Label>{children}</Style.Label>
+        <Style.Label $labelStyle={labelStyle}>{children}</Style.Label>
         {isRequired && <Style.RequiredMark>*</Style.RequiredMark>}
       </Style.Row>
       {labelElement && labelElement}
