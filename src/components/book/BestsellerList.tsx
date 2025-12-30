5fde4767f26bc103776aa481f31873ce621e5444
유림 <yulimh001@gmail.com>
2025-12-30 15:50:29 +0900

    feat: Prop누락 추가

--- src/components/book/BestsellerList.tsx
+++ src/components/book/BestsellerList.tsx
@@ -6,11 +6,16 @@ import { StyleBestSellerBookRowContainer } from '@/styles/common/Book.styles'
 export default function BestsellerList() {
   return (
     <StyleBestSellerBookRowContainer>
-      <BestSellerItem title={'책제목책제목책제목책제목책제목책제목'} author={'작가'} bookImage={'/bookimage.png'} />
-      <BestSellerItem title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
-      <BestSellerItem title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
-      <BestSellerItem title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
-      <BestSellerItem title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
+      <BestSellerItem
+        isbn={'1'}
+        title={'책제목책제목책제목책제목책제목책제목'}
+        author={'작가'}
+        bookImage={'/bookimage.png'}
+      />
+      <BestSellerItem isbn={'1'} title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
+      <BestSellerItem isbn={'1'} title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
+      <BestSellerItem isbn={'1'} title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
+      <BestSellerItem isbn={'1'} title={'책제목'} author={'작가'} bookImage={'/bookimage.png'} />
     </StyleBestSellerBookRowContainer>
   )
 }
