eef6dead548bab808053b434cc4a057413794f7b
유림 <yulimh001@gmail.com>
2025-12-30 15:39:42 +0900

    feat: review import문, 스타일

--- src/components/book/index.ts
+++ src/components/book/index.ts
@@ -5,5 +5,14 @@ export { default as PopularBook } from './PopularBook'
 export { default as WriteButton } from './WriteButton'
 export { default as BookContent } from './BookContent'
 export { default as BookSearchResults } from './BookSearchResults'
+export { default as PopularBookDescription } from './PopularBookDescription'
 export { default as BestSellerLoadMoreButton } from './BestSellerLoadMoreButton'
+
+export { default as BestSellerItem } from './BestSellerItem'
 export { default as BookIntroduction } from './BookIntroduction'
+
+//review
+export { default as BookQuote } from './review/BookQuote'
+export { default as ReviewSummary } from './review/ReviewSummary'
+export { default as ReviewItem } from './review/ReviewItem'
+export { default as ViewAllReviewsButton } from './ViewAllReviewsButton'
---
+++ src/styles/book/Review.styles.tsx
@@ -0,0 +1,117 @@
+import styled from '@emotion/styled'
+import { CSSObject } from '@emotion/react'
+
+//summary
+export const StyleReviewSummaryContainer = styled.div`
+  display: flex;
+  column-gap: 4px;
+`
+
+export const StyleReviewSummaryContent = styled.div`
+  display: flex;
+  column-gap: 4px;
+  align-items: center;
+  ${({ theme }) => theme.typography.buttonMd as CSSObject}
+  color: ${({ theme }) => theme.colors.textFieldDefaultText};
+`
+
+export const StyleReviewSummaryRating = styled.p`
+  ${({ theme }) => theme.typography.subtitleMd as CSSObject}
+  color: ${({ theme }) => theme.colors.headerText};
+`
+
+export const StyleReviewItemContainer = styled.div`
+  display: flex;
+  column-gap: 12px;
+  width: 100%;
+`
+
+//header
+export const ReviewHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`
+export const StyleReviewHeaderMetaNickName = styled.p`
+  ${({ theme }) => theme.typography.badgeMd as CSSObject}
+  color: ${({ theme }) => theme.colors.headerText};
+`
+export const StyleReviewHeaderMetaTime = styled.p`
+  ${({ theme }) => theme.typography.small as CSSObject}
+  color: ${({ theme }) => theme.colors.textFieldDefaultText};
+`
+
+export const StyleReviewHeaderMetaContainer = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 8px;
+`
+export const StyleReviewHeaderMetaUserInfo = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 4px;
+`
+
+//content
+export const StyleReviewContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const StyleReviewContent = styled.div`
+  ${({ theme }) => theme.typography.subtitleMd as CSSObject}
+  color: ${({ theme }) => theme.colors.headerText};
+`
+
+export const StyleReviewContentMeta = styled.div`
+  display: flex;
+  column-gap: 8px;
+  align-items: center;
+  ${({ theme }) => theme.typography.badgeSm as CSSObject}
+`
+
+export const StyleReviewContentMetaDate = styled.div`
+  display: flex;
+  column-gap: 8px;
+  ${({ theme }) => theme.typography.badgeSm as CSSObject}
+  color: ${({ theme }) => theme.colors.textFieldDefaultText};
+`
+
+//인용구
+export const StyleBookQuoteRowContainer = styled.div`
+  display: flex;
+  column-gap: 4px;
+  margin-left: 2px;
+  padding-left: 8px;
+  position: relative;
+  z-index: 1;
+  transition: all 0.2s ease-in-out;
+  background-color: transparent;
+  /* 연필 효과를 입힐 가상 요소 */
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    filter: url('#pencil-texture');
+    pointer-events: none;
+    box-sizing: border-box;
+    border-left: 1px solid ${({ theme }) => theme.colors.textFieldFilledLine};
+  }
+`
+export const StyleBookQuoteColumnContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export const StyleQuoteContent = styled.div`
+  ${({ theme }) => theme.typography.badgeMd as CSSObject}
+  color: ${({ theme }) => theme.colors.textFieldFilledLine};
+`
+export const StyleQuotePage = styled.div`
+  ${({ theme }) => theme.typography.badgeSm as CSSObject}
+  color: ${({ theme }) => theme.colors.textFieldDefaultLine};
+`
