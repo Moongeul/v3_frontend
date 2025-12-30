de12143f4ddb6e03a733c2134de3ed8a76c34513
유림 <yulimh001@gmail.com>
2025-12-30 15:42:09 +0900

    fix: 폰트 타입 지정

--- src/styles/Write.styles.tsx
+++ src/styles/Write.styles.tsx
@@ -1,15 +1,17 @@
 import styled from '@emotion/styled'
+import { CSSObject } from '@emotion/react'
 
 export const WritingGuide = styled.p`
-  ${({ theme }) => theme.typography.badgeMd}
+  ${({ theme }) => theme.typography.badgeMd as CSSObject}
   color: ${({ theme }) => theme.colors.buttonActiveGhost};
 `
 
-export const StarRatingColumn = styled.div<{ $textColor?: string }>`
+export const StarRatingColumn = styled.div<{ $size?: 'md' | 'sm'; $textColor?: string }>`
   display: flex;
   column-gap: 2px;
   color: ${({ $textColor }) => $textColor};
-  ${({ theme }) => theme.typography.badgeMd};
+  ${({ theme, $size }) =>
+    $size === 'md' ? (theme.typography.badgeMd as CSSObject) : (theme.typography.badgeSm as CSSObject)};
   align-items: center;
 `
 
@@ -22,7 +24,7 @@ export const QuoteTopElementContainer = styled.div`
   padding-top: 16px;
   padding-left: 12px;
   padding-right: 12px;
-  ${({ theme }) => theme.typography.subtitleMd}
+  ${({ theme }) => theme.typography.subtitleMd as CSSObject}
   color: ${({ theme }) => theme.colors.baseColor.gray300};
 `
 export const Column = styled.div`
@@ -30,7 +32,7 @@ export const Column = styled.div`
   column-gap: 2px;
 `
 export const QuotePageInput = styled.input`
-  ${({ theme }) => theme.typography.subtitleMd}
+  ${({ theme }) => theme.typography.subtitleMd as CSSObject}
   color: ${({ theme }) => theme.colors.baseColor.gray300};
   outline: none;
   width: fit-content;
