365fe8abdada180ebf61c071a276cbe4c3631a3a
유림 <yulimh001@gmail.com>
2026-03-14 20:48:56 +0900

    fix: 스토리버튼 내 게시물일 경우에만 보이도록 수정

--- src/app/[recordId]/layout.tsx
+++ src/app/[recordId]/layout.tsx
@@ -1,5 +1,8 @@
 import { Header, PageLayout, Spacing } from '@/components/common'
 import { StoryWriteButton } from '@/components/story'
+import { serverFetchPostDetail } from '@/lib/server/record'
+import { cookies } from 'next/headers'
+
 export default async function RecordDetailLayout({
   children,
   params,
@@ -8,10 +11,20 @@ export default async function RecordDetailLayout({
   params: Promise<{ recordId: string }>
 }>) {
   const { recordId } = await params
+
+  // 2. 쿠키 인스턴스 가져오기 (비동기 처리 필요 - Next.js 15 기준)
+  const cookieStore = await cookies()
+  const memberId = cookieStore.get('memberId')?.value
+
+  const result = await serverFetchPostDetail(Number(recordId))
+  const postData = result.data
+
   return (
     <div>
-      <Header headerType={'dynamic'} rightIcon={<StoryWriteButton recordId={recordId} />} />
-
+      <Header
+        headerType={'dynamic'}
+        rightIcon={Number(memberId) === postData.memberInfo.memberId ? <StoryWriteButton recordId={recordId} /> : null}
+      />
       <Spacing height={60} />
       <PageLayout>{children}</PageLayout>
     </div>
