ddada7645ff19a7c6028560e7dc2d7be16b787f5
유림 <yulimh001@gmail.com>
2026-04-14 13:38:16 +0900

    fix: 모달창이 안닫히는 버그 수정

--- src/components/common/modal/HomeModal.tsx
+++ src/components/common/modal/HomeModal.tsx
@@ -8,14 +8,13 @@ export default function HomeModal() {
   const { setModal, modals } = useModalStore((state) => state)
 
   useEffect(() => {
-    // 1. 쿠키에서 isReadingTaste 가져오기 (문자열 'true'로 저장됨)
     const isReadingTaste = Cookies.get('isReadingTaste')
 
-    // 2. 값이 'true'인 경우 모달 열기
-    if (isReadingTaste === 'false') {
+    // 쿠키가 없으면 (테스트 미완료) 모달 열기
+    if (!isReadingTaste) {
       setModal('isTestModalOpen', true)
     }
-  }, [modals.isTestModalOpen])
+  }, []) // 빈 배열: 마운트 시 1번만 실행
 
   return modals.isTestModalOpen ? <TestModal /> : null
 }
